390d392d3eb3a550bdd2c869c4d7d00b6d177a78
May Isrow <may.isrow@gmail.com>
2023-10-12 12:16:19 -0400

    A lot of refactoring, moved everything out into its own file, added color types that making saving and using colors a lot easier

---
+++ bogPhoneOS/Types/BogshAppType.swift
@@ -0,0 +1,8 @@
+//
+//  BogshAppType.swift
+//  bogPhoneOS
+//
+//  Created by May Isrow on 10/12/23.
+//
+
+import Foundation
---
+++ bogPhoneOS/Types/BogshColorType.swift
@@ -0,0 +1,8 @@
+//
+//  BogshColorType.swift
+//  bogPhoneOS
+//
+//  Created by May Isrow on 10/12/23.
+//
+
+import Foundation
