39772b246f84ef5290fd74a97d7943137b568c6d
May Isrow <may.isrow@gmail.com>
2023-10-18 21:30:26 -0400

    Fix exmpty image string bug

--- bogPhoneOS/Views/Components/HomeIcon.swift
+++ bogPhoneOS/Views/Components/HomeIcon.swift
@@ -9,21 +9,25 @@ import SwiftUI
 
 struct HomeIcon: View {
     
-    @State var iconSystemName: String = ""
+    @State var iconSystemName: String?
     @State var displayName: String = ""
     @State var colorType: BogshColorType = .lightGray
     
     var body: some View {
         VStack {
-            Image(systemName: iconSystemName)
-                .resizable()
-                .padding()
-                .frame(width: 80, height: 80)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(colorType.rawValue), lineWidth: 4)
-                )
-                .foregroundStyle(Color(colorType.rawValue))
+            VStack {
+                if let systemName = iconSystemName {
+                    Image(systemName: systemName)
+                        .resizable()
+                        .padding()
+                }
+            }
+            .frame(width: 80, height: 80)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(colorType.rawValue), lineWidth: 4)
+            )
+            .foregroundStyle(Color(colorType.rawValue))
             Text(displayName)
                 .modifier(MinimalModifier())
         }
